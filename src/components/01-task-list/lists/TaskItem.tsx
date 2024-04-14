6c05621f0a52848ea4f8467e7883f10efd10937d
Alejandro Coca <alxmcr@outlook.com>
2024-04-14 00:30:42 -0400

    wip

--- src/components/01-task-list/lists/TaskItem.tsx
+++ src/components/01-task-list/lists/TaskItem.tsx
@@ -1,22 +1,41 @@
+import React from 'react';
 import { TaskData } from '../../../@types/appTypes';
+import { TasksDispatchContext } from '../../../providers/TasksContext';
 
 type Props = {
   task: TaskData;
 };
 
 export default function TaskItem({ task }: Props) {
+  const dispatch = React.useContext(TasksDispatchContext);
+
   const handleDeleteTask = (taskId = '') => {
     console.log('delete tasks...', taskId);
+    dispatch({
+      type: 'deleted',
+      payload: {
+        id: taskId,
+      },
+    });
   };
 
-  const handleChangeTask = (taskToChange: TaskData) => {
-    console.log('task to change', taskToChange);
+  const handleChecked = (ev: React.ChangeEvent<HTMLInputElement>) => {
+    dispatch({
+      type: 'changed',
+      payload: { ...task, done: ev.target.checked },
+    });
   };
 
   return (
     <li className="flex items-center justify-between gap-3">
       <label htmlFor={task.id} className="flex items-center gap-3">
-        <input type="checkbox" name={task.id} id={task.id} className="size-8" />
+        <input
+          type="checkbox"
+          name={task.id}
+          id={task.id}
+          className="size-8"
+          onChange={handleChecked}
+        />
         <span>{task.name}</span>
       </label>
       <div className="flex items-center gap-4">
